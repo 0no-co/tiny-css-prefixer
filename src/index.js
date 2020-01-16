d80b84b9e92964801939c9e1afba90aa8a764696
Phil Pluckthun <phil@kitten.sh>
2020-01-16 00:19:47 +0000

    Add cache to prefixProperty

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import /* codegen */ '../scripts/generate-prefix-map';
 const ms = 0b001;
 const moz = 0b010;
 const webkit = 0b100;
+const cache = {};
 
 export const prefixProperty = prop => {
-  return (msPrefixRe.test(prop) ? ms : 0)
+  if (cache[prop]) return cache[prop];
+  return (cache[prop] = (msPrefixRe.test(prop) ? ms : 0)
     | (mozPrefixRe.test(prop) ? moz : 0)
-    | (webkitPrefixRe.test(prop) ? webkit : 0)
+    | (webkitPrefixRe.test(prop) ? webkit : 0));
 };
 
 export const prefixValue = (prop, value) => {
